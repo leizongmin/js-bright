4c3f41e7a8ccd483db96501710ebf50328f60a9f
Lei Zongmin <leizongmin@gmail.com>
2012-11-13 18:09:42 +0800

    输出单词数组，属性名称更改：word => text

--- test/compiler/test_token.js
+++ test/compiler/test_token.js
@@ -23,17 +23,17 @@ describe('compiler/token', function () {
     };
 
     it('正常数字', function () {
-      testEql('123', [{type: WORD.NUMBER, line: 0, column: 0, word: '123'}]);
-      testEql('123.456', [{type: WORD.NUMBER, line: 0, column: 0, word: '123.456'}]);
-      testEql('123e123', [{type: WORD.NUMBER, line: 0, column: 0, word: '123e123'}]);
-      testEql('123e123.456', [{type: WORD.NUMBER, line: 0, column: 0, word: '123e123.456'}]);
-      testEql('123E123', [{type: WORD.NUMBER, line: 0, column: 0, word: '123E123'}]);
-      testEql('123.E123', [{type: WORD.NUMBER, line: 0, column: 0, word: '123.E123'}]);
-      testEql('123e+123', [{type: WORD.NUMBER, line: 0, column: 0, word: '123e+123'}]);
-      testEql('123e-123', [{type: WORD.NUMBER, line: 0, column: 0, word: '123e-123'}]);
-      testEql('123.456e123', [{type: WORD.NUMBER, line: 0, column: 0, word: '123.456e123'}]);
-      testEql('123.456e123.456', [{type: WORD.NUMBER, line: 0, column: 0, word: '123.456e123.456'}]);
-      testEql('123.456e-123.456', [{type: WORD.NUMBER, line: 0, column: 0, word: '123.456e-123.456'}]);
+      testEql('123', [{type: WORD.NUMBER, line: 0, column: 0, text: '123'}]);
+      testEql('123.456', [{type: WORD.NUMBER, line: 0, column: 0, text: '123.456'}]);
+      testEql('123e123', [{type: WORD.NUMBER, line: 0, column: 0, text: '123e123'}]);
+      testEql('123e123.456', [{type: WORD.NUMBER, line: 0, column: 0, text: '123e123.456'}]);
+      testEql('123E123', [{type: WORD.NUMBER, line: 0, column: 0, text: '123E123'}]);
+      testEql('123.E123', [{type: WORD.NUMBER, line: 0, column: 0, text: '123.E123'}]);
+      testEql('123e+123', [{type: WORD.NUMBER, line: 0, column: 0, text: '123e+123'}]);
+      testEql('123e-123', [{type: WORD.NUMBER, line: 0, column: 0, text: '123e-123'}]);
+      testEql('123.456e123', [{type: WORD.NUMBER, line: 0, column: 0, text: '123.456e123'}]);
+      testEql('123.456e123.456', [{type: WORD.NUMBER, line: 0, column: 0, text: '123.456e123.456'}]);
+      testEql('123.456e-123.456', [{type: WORD.NUMBER, line: 0, column: 0, text: '123.456e-123.456'}]);
     });
 
     it('异常数字', function () {
@@ -46,10 +46,10 @@ describe('compiler/token', function () {
     });
 
     it('正常字符串', function () {
-      testEql('"hello=jkfjkfs"', [{type: WORD.STRING, line: 0, column: 0, word: '"hello=jkfjkfs"'}]);
-      testEql('"hello=\nd\njkfjkfs"', [{type: WORD.STRING, line: 0, column: 0, word: '"hello=\nd\njkfjkfs"'}]);
-      testEql('"hello=\\\"jkfjkfs"', [{type: WORD.STRING, line: 0, column: 0, word: '"hello=\\\"jkfjkfs"'}]);
-      testEql('"abc\\"\'\r\n\t\\""', [{type: WORD.STRING, line: 0, column: 0, word: '"abc\\"\'\r\n\t\\""'}]);
+      testEql('"hello=jkfjkfs"', [{type: WORD.STRING, line: 0, column: 0, text: '"hello=jkfjkfs"'}]);
+      testEql('"hello=\nd\njkfjkfs"', [{type: WORD.STRING, line: 0, column: 0, text: '"hello=\nd\njkfjkfs"'}]);
+      testEql('"hello=\\\"jkfjkfs"', [{type: WORD.STRING, line: 0, column: 0, text: '"hello=\\\"jkfjkfs"'}]);
+      testEql('"abc\\"\'\r\n\t\\""', [{type: WORD.STRING, line: 0, column: 0, text: '"abc\\"\'\r\n\t\\""'}]);
     });
 
     it('异常字符串', function () {
@@ -57,20 +57,20 @@ describe('compiler/token', function () {
     });
 
     it('符号', function () {
-      testEql('+-*/', [{type: WORD.SYMBLE, line: 0, column: 0, word: '+'},
-                       {type: WORD.SYMBLE, line: 0, column: 1, word: '-'},
-                       {type: WORD.SYMBLE, line: 0, column: 2, word: '*'},
-                       {type: WORD.SYMBLE, line: 0, column: 3, word: '/'}]);
+      testEql('+-*/', [{type: WORD.SYMBLE, line: 0, column: 0, text: '+'},
+                       {type: WORD.SYMBLE, line: 0, column: 1, text: '-'},
+                       {type: WORD.SYMBLE, line: 0, column: 2, text: '*'},
+                       {type: WORD.SYMBLE, line: 0, column: 3, text: '/'}]);
     });
 
     it('正常单词', function () {
-      testEql('hello', [{type: WORD.NAME, line: 0, column: 0, word: 'hello'}]);
-      testEql('hello34', [{type: WORD.NAME, line: 0, column: 0, word: 'hello34'}]);
-      testEql('hello_s', [{type: WORD.NAME, line: 0, column: 0, word: 'hello_s'}]);
-      testEql('_hello', [{type: WORD.NAME, line: 0, column: 0, word: '_hello'}]);
-      testEql('$', [{type: WORD.NAME, line: 0, column: 0, word: '$'}]);
-      testEql('_', [{type: WORD.NAME, line: 0, column: 0, word: '_'}]);
-      testEql('$$_f', [{type: WORD.NAME, line: 0, column: 0, word: '$$_f'}]);
+      testEql('hello', [{type: WORD.NAME, line: 0, column: 0, text: 'hello'}]);
+      testEql('hello34', [{type: WORD.NAME, line: 0, column: 0, text: 'hello34'}]);
+      testEql('hello_s', [{type: WORD.NAME, line: 0, column: 0, text: 'hello_s'}]);
+      testEql('_hello', [{type: WORD.NAME, line: 0, column: 0, text: '_hello'}]);
+      testEql('$', [{type: WORD.NAME, line: 0, column: 0, text: '$'}]);
+      testEql('_', [{type: WORD.NAME, line: 0, column: 0, text: '_'}]);
+      testEql('$$_f', [{type: WORD.NAME, line: 0, column: 0, text: '$$_f'}]);
     });
 
     it('异常单词', function () {
@@ -78,63 +78,65 @@ describe('compiler/token', function () {
     });
 
     it('单行注释', function () {
-      testEql('//abcd', [{type: WORD.COMMENT, line: 0, column: 0, word: '//abcd'}]);
-      testEql('//abcd//sss', [{type: WORD.COMMENT, line: 0, column: 0, word: '//abcd//sss'}]);
-      testEql('//abc\nabc', [{type: WORD.COMMENT, line: 0, column: 0, word: '//abc'},
-                             {type: WORD.NAME, line: 1, column: 0, word: 'abc'}]);
+      testEql('//abcd', [{type: WORD.COMMENT, line: 0, column: 0, text: '//abcd'}]);
+      testEql('//abcd//sss', [{type: WORD.COMMENT, line: 0, column: 0, text: '//abcd//sss'}]);
+      testEql('//abc\nabc', [{type: WORD.COMMENT, line: 0, column: 0, text: '//abc'},
+                             {type: WORD.NAME, line: 1, column: 0, text: 'abc'}]);
     });
 
     it('多行注释', function () {
-      testEql('/*hello*/', [{type: WORD.COMMENT, line: 0, column: 0, word: '/*hello*/'}]);
-      testEql('/*how*//*are*//*you*/', [{type: WORD.COMMENT, line: 0, column: 0, word: '/*how*/'},
-                                        {type: WORD.COMMENT, line: 0, column: 7, word: '/*are*/'},
-                                        {type: WORD.COMMENT, line: 0, column: 14, word: '/*you*/'}]);
+      testEql('/*hello*/', [{type: WORD.COMMENT, line: 0, column: 0, text: '/*hello*/'}]);
+      testEql('/*how*//*are*//*you*/', [{type: WORD.COMMENT, line: 0, column: 0, text: '/*how*/'},
+                                        {type: WORD.COMMENT, line: 0, column: 7, text: '/*are*/'},
+                                        {type: WORD.COMMENT, line: 0, column: 14, text: '/*you*/'}]);
     });
 
     it('单行综合测试', function () {
-      testEql('if a + b = 10', [{type: WORD.NAME, line: 0, column: 0, word: 'if'},
-                                {type: WORD.NAME, line: 0, column: 3, word: 'a'},
-                                {type: WORD.SYMBLE, line: 0, column: 5, word: '+'},
-                                {type: WORD.NAME, line: 0, column: 7, word: 'b'},
-                                {type: WORD.SYMBLE, line: 0, column: 9, word: '='},
-                                {type: WORD.NUMBER, line: 0, column: 11, word: '10'},]);
-      testEql('if a+b=10', [{type: WORD.NAME, line: 0, column: 0, word: 'if'},
-                            {type: WORD.NAME, line: 0, column: 3, word: 'a'},
-                            {type: WORD.SYMBLE, line: 0, column: 4, word: '+'},
-                            {type: WORD.NAME, line: 0, column: 5, word: 'b'},
-                            {type: WORD.SYMBLE, line: 0, column: 6, word: '='},
-                            {type: WORD.NUMBER, line: 0, column: 7, word: '10'},]);
-      testEql('"abc" + 50 = "abc50"', [{type: WORD.STRING, line: 0, column: 0, word: '"abc"'},
-                                       {type: WORD.SYMBLE, line: 0, column: 6, word: '+'},
-                                       {type: WORD.NUMBER, line: 0, column: 8, word: '50'},
-                                       {type: WORD.SYMBLE, line: 0, column: 11, word: '='},
-                                       {type: WORD.STRING, line: 0, column: 13, word: '"abc50"'}]);
-      testEql('"abc"+50="abc50"', [{type: WORD.STRING, line: 0, column: 0, word: '"abc"'},
-                                    {type: WORD.SYMBLE, line: 0, column: 5, word: '+'},
-                                    {type: WORD.NUMBER, line: 0, column: 6, word: '50'},
-                                    {type: WORD.SYMBLE, line: 0, column: 8, word: '='},
-                                    {type: WORD.STRING, line: 0, column: 9, word: '"abc50"'}]);
-      //testEql('abc;//abc', [{type: WORD.NAME, }])
+      testEql('if a + b = 10', [{type: WORD.NAME, line: 0, column: 0, text: 'if'},
+                                {type: WORD.NAME, line: 0, column: 3, text: 'a'},
+                                {type: WORD.SYMBLE, line: 0, column: 5, text: '+'},
+                                {type: WORD.NAME, line: 0, column: 7, text: 'b'},
+                                {type: WORD.SYMBLE, line: 0, column: 9, text: '='},
+                                {type: WORD.NUMBER, line: 0, column: 11, text: '10'},]);
+      testEql('if a+b=10', [{type: WORD.NAME, line: 0, column: 0, text: 'if'},
+                            {type: WORD.NAME, line: 0, column: 3, text: 'a'},
+                            {type: WORD.SYMBLE, line: 0, column: 4, text: '+'},
+                            {type: WORD.NAME, line: 0, column: 5, text: 'b'},
+                            {type: WORD.SYMBLE, line: 0, column: 6, text: '='},
+                            {type: WORD.NUMBER, line: 0, column: 7, text: '10'},]);
+      testEql('"abc" + 50 = "abc50"', [{type: WORD.STRING, line: 0, column: 0, text: '"abc"'},
+                                       {type: WORD.SYMBLE, line: 0, column: 6, text: '+'},
+                                       {type: WORD.NUMBER, line: 0, column: 8, text: '50'},
+                                       {type: WORD.SYMBLE, line: 0, column: 11, text: '='},
+                                       {type: WORD.STRING, line: 0, column: 13, text: '"abc50"'}]);
+      testEql('"abc"+50="abc50"', [{type: WORD.STRING, line: 0, column: 0, text: '"abc"'},
+                                    {type: WORD.SYMBLE, line: 0, column: 5, text: '+'},
+                                    {type: WORD.NUMBER, line: 0, column: 6, text: '50'},
+                                    {type: WORD.SYMBLE, line: 0, column: 8, text: '='},
+                                    {type: WORD.STRING, line: 0, column: 9, text: '"abc50"'}]);
+      testEql('abc;//abc', [{type: WORD.NAME, line: 0, column: 0, text: 'abc'},
+                            {type: WORD.SYMBLE, line: 0, column: 3, text: ';'},
+                            {type: WORD.COMMENT, line: 0, column: 4, text: '//abc'}])
     });
 
     it('多行综合测试', function () {
-      testEql('if (a+10="123") {\n\tok()\n} else {\n\texit\n}', [{type: WORD.NAME, line: 0, column: 0, word: 'if'},
-                                                                 {type: WORD.SYMBLE, line: 0, column: 3, word: '('},
-                                                                 {type: WORD.NAME, line: 0, column: 4, word: 'a'},
-                                                                 {type: WORD.SYMBLE, line: 0, column: 5, word: '+'},
-                                                                 {type: WORD.NUMBER, line: 0, column: 6, word: '10'},
-                                                                 {type: WORD.SYMBLE, line: 0, column: 8, word: '='},
-                                                                 {type: WORD.STRING, line: 0, column: 9, word: '"123"'},
-                                                                 {type: WORD.SYMBLE, line: 0, column: 14, word: ')'},
-                                                                 {type: WORD.SYMBLE, line: 0, column: 16, word: '{'},
-                                                                 {type: WORD.NAME, line: 1, column: 1, word: 'ok'},
-                                                                 {type: WORD.SYMBLE, line: 1, column: 3, word: '('},
-                                                                 {type: WORD.SYMBLE, line: 1, column: 4, word: ')'},
-                                                                 {type: WORD.SYMBLE, line: 2, column: 0, word: '}'},
-                                                                 {type: WORD.NAME, line: 2, column: 2, word: 'else'},
-                                                                 {type: WORD.SYMBLE, line: 2, column: 7, word: '{'},
-                                                                 {type: WORD.NAME, line: 3, column: 1, word: 'exit'},
-                                                                 {type: WORD.SYMBLE, line: 4, column: 0, word: '}'},]);
+      testEql('if (a+10="123") {\n\tok()\n} else {\n\texit\n}', [{type: WORD.NAME, line: 0, column: 0, text: 'if'},
+                                                                 {type: WORD.SYMBLE, line: 0, column: 3, text: '('},
+                                                                 {type: WORD.NAME, line: 0, column: 4, text: 'a'},
+                                                                 {type: WORD.SYMBLE, line: 0, column: 5, text: '+'},
+                                                                 {type: WORD.NUMBER, line: 0, column: 6, text: '10'},
+                                                                 {type: WORD.SYMBLE, line: 0, column: 8, text: '='},
+                                                                 {type: WORD.STRING, line: 0, column: 9, text: '"123"'},
+                                                                 {type: WORD.SYMBLE, line: 0, column: 14, text: ')'},
+                                                                 {type: WORD.SYMBLE, line: 0, column: 16, text: '{'},
+                                                                 {type: WORD.NAME, line: 1, column: 1, text: 'ok'},
+                                                                 {type: WORD.SYMBLE, line: 1, column: 3, text: '('},
+                                                                 {type: WORD.SYMBLE, line: 1, column: 4, text: ')'},
+                                                                 {type: WORD.SYMBLE, line: 2, column: 0, text: '}'},
+                                                                 {type: WORD.NAME, line: 2, column: 2, text: 'else'},
+                                                                 {type: WORD.SYMBLE, line: 2, column: 7, text: '{'},
+                                                                 {type: WORD.NAME, line: 3, column: 1, text: 'exit'},
+                                                                 {type: WORD.SYMBLE, line: 4, column: 0, text: '}'},]);
     });
   });
 
@@ -147,29 +149,29 @@ describe('compiler/token', function () {
     };
 
     it('关键词处理 - 正常情况', function () {
-      testEql('false', [{type: WORD.KEYWORD, line: 0, column: 0, word: 'false'}]);
-      testEql('false.true', [{type: WORD.IDENTIFIER, line: 0, column: 0, word: 'false'},
-                             {type: WORD.SYMBLE, line: 0, column: 5, word: '.'},
-                             {type: WORD.IDENTIFIER, line: 0, column: 6, word: 'true'}]);
-      testEql('{false:true}', [{type: WORD.SYMBLE, line: 0, column: 0, word: '{'},
-                               {type: WORD.IDENTIFIER, line: 0, column: 1, word: 'false'},
-                               {type: WORD.SYMBLE, line: 0, column: 6, word: ':'},
-                               {type: WORD.KEYWORD, line: 0, column: 7, word: 'true'},
-                               {type: WORD.SYMBLE, line: 0, column: 11, word: '}'}]);
-      testEql('false?true:false', [{type: WORD.KEYWORD, line: 0, column: 0, word: 'false'},
-                                  {type: WORD.SYMBLE, line: 0, column: 5, word: '?'},
-                                  {type: WORD.KEYWORD, line: 0, column: 6, word: 'true'},
-                                  {type: WORD.SYMBLE, line: 0, column: 10, word: ':'},
-                                  {type: WORD.KEYWORD, line: 0, column: 11, word: 'false'}]);
-      testEql('{do:do,try:try}', [{type: WORD.SYMBLE, line: 0, column: 0, word: '{'},
-                                  {type: WORD.IDENTIFIER, line: 0, column: 1, word: 'do'},
-                                  {type: WORD.SYMBLE, line: 0, column: 3, word: ':'},
-                                  {type: WORD.KEYWORD, line: 0, column: 4, word: 'do'},
-                                  {type: WORD.SYMBLE, line: 0, column: 6, word: ','},
-                                  {type: WORD.IDENTIFIER, line: 0, column: 7, word: 'try'},
-                                  {type: WORD.SYMBLE, line: 0, column: 10, word: ':'},
-                                  {type: WORD.KEYWORD, line: 0, column: 11, word: 'try'},
-                                  {type: WORD.SYMBLE, line: 0, column: 14, word: '}'}]);
+      testEql('false', [{type: WORD.KEYWORD, line: 0, column: 0, text: 'false'}]);
+      testEql('false.true', [{type: WORD.IDENTIFIER, line: 0, column: 0, text: 'false'},
+                             {type: WORD.SYMBLE, line: 0, column: 5, text: '.'},
+                             {type: WORD.IDENTIFIER, line: 0, column: 6, text: 'true'}]);
+      testEql('{false:true}', [{type: WORD.SYMBLE, line: 0, column: 0, text: '{'},
+                               {type: WORD.IDENTIFIER, line: 0, column: 1, text: 'false'},
+                               {type: WORD.SYMBLE, line: 0, column: 6, text: ':'},
+                               {type: WORD.KEYWORD, line: 0, column: 7, text: 'true'},
+                               {type: WORD.SYMBLE, line: 0, column: 11, text: '}'}]);
+      testEql('false?true:false', [{type: WORD.KEYWORD, line: 0, column: 0, text: 'false'},
+                                  {type: WORD.SYMBLE, line: 0, column: 5, text: '?'},
+                                  {type: WORD.KEYWORD, line: 0, column: 6, text: 'true'},
+                                  {type: WORD.SYMBLE, line: 0, column: 10, text: ':'},
+                                  {type: WORD.KEYWORD, line: 0, column: 11, text: 'false'}]);
+      testEql('{do:do,try:try}', [{type: WORD.SYMBLE, line: 0, column: 0, text: '{'},
+                                  {type: WORD.IDENTIFIER, line: 0, column: 1, text: 'do'},
+                                  {type: WORD.SYMBLE, line: 0, column: 3, text: ':'},
+                                  {type: WORD.KEYWORD, line: 0, column: 4, text: 'do'},
+                                  {type: WORD.SYMBLE, line: 0, column: 6, text: ','},
+                                  {type: WORD.IDENTIFIER, line: 0, column: 7, text: 'try'},
+                                  {type: WORD.SYMBLE, line: 0, column: 10, text: ':'},
+                                  {type: WORD.KEYWORD, line: 0, column: 11, text: 'try'},
+                                  {type: WORD.SYMBLE, line: 0, column: 14, text: '}'}]);
     });
 
   });
