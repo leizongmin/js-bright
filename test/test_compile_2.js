4253f2ee31c4b8d15827e483e90353cc06fce5c8
Lei Zongmin <leizongmin@gmail.com>
2012-12-03 15:07:20 +0800

    修正运行时库中，if..elseif条件满足后，仍然继续执行后面的elseif问题

--- test/test_compile_2.js
+++ test/test_compile_2.js
@@ -16,7 +16,7 @@ describe('compile 2', function () {
     var source = fs.readFileSync(filename, 'utf8');
     return compiler.compile(source);
   };
-  /*
+  
   it('#1 嵌套for / if', function (done) {
     var fn = compile('1');
     fn(function (err, sum, i, j, k) {
@@ -69,14 +69,14 @@ describe('compile 2', function () {
       })  
     })
   });
-  */
+  
   it('#6 for in 里面的 if elseif', function (done) {
     var fn = compile('6');
-    console.log(fn.toString());
-    fn(function (err, ret1, ret2) {
+    fn(function (err, ret1, ret2, ret3) {
       should.equal(err, null);
       ret1.should.eql([2,4,6,8,10]);
       ret2.should.eql([3,9]);
+      ret3.should.eql([1,5,7]);
       done();
     });
   });
